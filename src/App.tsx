531b541537f61378cd6826f3699b0c601628077c
Le Thanh Long <longlt2@vmogroup.com>
2025-05-28 17:39:02 +0700

    create header + responsive

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Banner } from './components/Banner/Banner'
+import { Header } from './components/Header/Header'
+
+function App() {
+
+  return (
+    <div className='min-w-[640px]'>
+      <Banner />
+      <Header />
+    </div>
+  )
+}
+
+export default App
